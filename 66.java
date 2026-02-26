5e0973f331dc1e7001b250e0534312d10e923c6b
void <golugurisandeepreddy5@gmail.com>
2026-02-26 21:17:06 +0530

    66.Plus One

---
+++ 66.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] d) {
+        int n = d.length;
+
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            if (d[i] < 9) 
+            {
+                d[i] += 1;
+                return d;
+            } 
+            else 
+            {
+                d[i] = 0;
+            }
+        }
+
+        int[] ans = new int[n + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
