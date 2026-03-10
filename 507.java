94fcfa9e50dbd2a75fa6b21c11647defc461c94a
void <golugurisandeepreddy5@gmail.com>
2026-03-10 21:27:44 +0530

    507. Perfect Number

---
+++ 507.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkPerfectNumber(int n) {
+        if(n<=1)
+        {
+            return false;
+        }
+        int sum=1;
+        for(int i=2;i<=Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                sum+=i;
+                int d=n/i;
+                if(d!=i)
+                {
+                    sum+=d;
+                }
+            }
+            
+        }
+        return sum==n;
+        
+    }
+}
