428dddb5ee26c73c739f3508e0b650368452a351
void <golugurisandeepreddy5@gmail.com>
2026-03-10 13:33:47 +0530

    198. House Robber

--- 198.java
+++ 198.java
@@ -1,19 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        
-        if(n == 1) return nums[0];
+        int prev1 = 0;
+        int prev2 = 0;
 
-        int a[] = new int[n];
-
-        a[0] = nums[0];
-        a[1] = Math.max(nums[0], nums[1]);
-
-        for(int i = 2; i < n; i++)
+        for(int num : nums)
         {
-            a[i] = Math.max(a[i-1], nums[i] + a[i-2]);
+            int temp = Math.max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = temp;
         }
 
-        return a[n-1];
+        return prev1;
     }
 }
