e3b85a6123e05c7ef135dc91ed4e0ff18692cb75
void <golugurisandeepreddy5@gmail.com>
2026-03-11 22:57:36 +0530

    492. Construct the Rectangle

--- 492.java
+++ 492.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] constructRectangle(int n) {
 
-        int[] ans = new int[2];
+        int[] a= new int[2];
 
         for(int i = (int)Math.sqrt(n); i >= 1; i--)
         {
             if(n % i == 0)
             {
-                ans[0] = n / i; 
-                ans[1] = i;  
+                a[0] = n / i; 
+                a[1] = i;  
                 break;
             }
         }
 
-        return ans;
+        return a;
     }
 }
